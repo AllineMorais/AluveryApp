78218e41f6f2783b994a828db53b3f2a7cfb3460
AllineMorais <153544392+AllineMorais@users.noreply.github.com>
2024-05-27 20:19:11 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.aluvery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aluvery.ui.theme.AluveryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent()
+        {
+            MyfristComposalbe()
+        }
+    }
+}
+
+@Composable
+fun MyfristComposalbe()
+{
+   Text(text = "Teste teste")
+}
+
+@Preview
+@Composable
+fun MyfristComposalbePreview()
+{
+    MyfristComposalbe()
+}
+
